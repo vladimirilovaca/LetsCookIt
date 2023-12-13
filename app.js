942ca3089182ebd48d93811db2ee12fb565c80a4
HisokaHX <luca.tobiello92@gmail.comgit config --global user.email luca.tobiello92@gmail.com>
2023-12-13 19:27:25 +0100

    session done

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
-const express = require("express"); 
+const express = require("express");
 const hbs = require("hbs");
 const logger = require("morgan");
 
-require("./config/db.config"); 
+require("./config/db.config");
 
-const app = express(); 
+const app = express();
 
 app.use(logger("dev"));
-app.use(express.urlencoded({ extended: true })); 
-app.use(express.static("public")); 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.set("views", __dirname + "/views"); 
-app.set("view engine", "hbs"); 
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
 
-hbs.registerPartials(__dirname + "/views/partials"); 
+hbs.registerPartials(__dirname + "/views/partials")
 
-const router = require("./router/router"); 
-app.use("/", router); 
+const { sessionConfig } = require("./config/session.config");
+app.use(sessionConfig);
+app.use((req, res, next) => {
+    res.locals.currentUser = req.session.currentUser;
+    next();
+});
+
+const router = require("./router/router");
+app.use("/", router);
 
 app.use((err, req, res, next) => {
-    console.error(err); 
+    console.error(err);
 
     if (err.status === 404) {
-        res.render('error', {tittle: err.message})
+        res.render('error', { tittle: err.message })
     } else {
         res.render('error');
     }
 })
 
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`App running at port ${port} 🚀🚀`));
--- router/router.js
+++ router/router.js
@@ -1,14 +1,19 @@
 const router = require("express").Router();
-const authController = require("../controllers/auth.controller"); 
+const authController = require("../controllers/auth.controller");
+const usersController = require("../controllers/user.controller")
 
 router.get("/", (req, res, next) => {
     res.render("home");
 });
 
 router.get("/login", authController.login);
-router.post("/logn", authController.doLogin);
+router.post("/login", authController.doLogin);
 router.get("/register", authController.register);
 router.post("/register", authController.doRegister);
+router.get("/logout", authController.logout);
+
+
+router.get("/profile", usersController.profile);
 
 
 module.exports = router;
