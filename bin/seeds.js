14836ee2b4c7f85e4a1d58e84e9b60adbb63f8c4
HisokaHX <luca.tobiello92@gmail.comgit config --global user.email luca.tobiello92@gmail.com>
2023-12-16 13:13:28 +0100

    json created

--- bin/seeds.js
+++ bin/seeds.js
@@ -3,4 +3,42 @@ const user = [{
     email: "luchita@letscookit.com",
     password: "12345678",
     isActive: true,
-}]
+}];
+
+const mongoose = require('mongoose');
+const Post = require('../models/Post.model');
+const User = require('../models/User.model');
+
+const posts = require('../public/js/posts.json');
+require('../config/db.config');
+
+mongoose.connection.once('open', () => {
+    mongoose.connection.dropDatabase()
+        .then(() => {
+            console.log('DB cleared');
+        })
+        .then(() => {
+            return User.create(user);
+        })
+        .then((usersDB) => {
+            const postsToCreate = [...posts].map(post => {
+                return {
+                    ...post,
+                    user: usersDB[0]._id
+                }
+            })
+            return Post.create(postsToCreate)
+        })
+        .then((postsCB) => {
+            postsCB.forEach(post => console.log(`${post.name} has been created`))
+        })
+        .catch(err => console.error(err))
+        .finally(() => {
+            mongoose.connection.close()
+                .then(() => {
+                    console.log('End of seeds');
+                })
+                .catch((err) => console.error('Error while disconnecting', err))
+                .finally(() => process.exit(0))
+        })
+})
